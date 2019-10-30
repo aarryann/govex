628bc88a9093ab96c6b1c238cfab4df8f9941bc2
Vivek Pillai <vpillai@ctisinc.com>
2019-10-30 16:14:19 -0400

    changed layout...

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -15,38 +15,60 @@
   <PreloadingIndicator />
 {/if}
 
-<div class="container-scroller">
-  {#if $page.path === '/login' || $page.path === '/register'}
-    <!--TODO: For some reason not equals not working hence put Nav segment in else clause-->
-    <div class="container-fluid page-body-wrapper full-page-wrapper">
-      <div class="main-panel">
-        <div class="content-wrapper d-flex align-items-center auth px-0">
-          <slot />
-        </div>
-        <!-- main-panel ends -->
+<!-- container-scroller from template.html-->
+{#if $page.path === '/login' || $page.path === '/register'}
+  <!--TODO: For some reason not equals not working hence put Nav segment in else clause-->
+  <div class="container-fluid page-body-wrapper full-page-wrapper">
+    <div class="main-panel">
+      <div class="content-wrapper d-flex align-items-center auth px-0">
+        <slot />
       </div>
-      <!-- content-wrapper ends -->
+      <!-- main-panel ends -->
     </div>
-    <!-- page-body-wrapper ends -->
+    <!-- content-wrapper ends -->
+  </div>
+  <!-- page-body-wrapper ends -->
+{:else}
+  {#if $session.user}
+    <ProtectedNav {segment} />
   {:else}
+    <Nav {segment} />
+  {/if}
+  <div class="container-fluid page-body-wrapper">
     {#if $session.user}
       <ThemeSettings {segment} />
       <SidebarRight {segment} />
       <SidebarLeft {segment} />
-      <ProtectedNav {segment} />
-    {:else}
-      <Nav {segment} />
     {/if}
-    <div class="container-fluid page-body-wrapper">
-      <div class="main-panel">
-        <div class="content-wrapper">
-          <slot />
-        </div>
-        <!-- main-panel ends -->
+    <div class="main-panel">
+      <div class="content-wrapper">
+        <slot />
       </div>
-      <!-- content-wrapper ends -->
+      <footer class="footer">
+        <div
+          class="d-sm-flex justify-content-center justify-content-sm-between">
+          <span
+            class="text-muted text-center text-sm-left d-block d-sm-inline-block">
+            Copyright © 2018
+            <a
+              href="https://www.urbanui.com/"
+              target="_blank"
+              class="text-muted">
+              Urbanui
+            </a>
+            . All rights reserved.
+          </span>
+          <span
+            class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">
+            Hand-crafted &amp; made with
+            <i class="mdi mdi-heart-outline text-primary" />
+          </span>
+        </div>
+      </footer>
+      <!-- main-panel ends -->
     </div>
-    <!-- page-body-wrapper ends -->
-  {/if}
-</div>
+    <!-- content-wrapper ends -->
+  </div>
+  <!-- page-body-wrapper ends -->
+{/if}
 <!-- container-scroller -->
