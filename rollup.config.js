fa6db4b1fee460708ac37d7b41a1b572009fe153
Vivek Pillai <vpillai@ctisinc.com>
2020-04-22 19:11:54 -0400

    clear errors...

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,34 @@
-import "dotenv/config";
-import replace from "@rollup/plugin-replace";
-import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
-import dotenvPlugin from "rollup-plugin-dotenv";
-import resolve from "rollup-plugin-node-resolve";
-import svelte from "rollup-plugin-svelte";
-import { terser } from "rollup-plugin-terser";
-import config from "sapper/config/rollup.js";
-import pkg from "./package.json";
-import sveltePreprocess from "svelte-preprocess";
+import 'dotenv/config';
+import replace from '@rollup/plugin-replace';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import dotenvPlugin from 'rollup-plugin-dotenv';
+import resolve from 'rollup-plugin-node-resolve';
+import svelte from 'rollup-plugin-svelte';
+import { terser } from 'rollup-plugin-terser';
+import config from 'sapper/config/rollup.js';
+import pkg from './package.json';
+import sveltePreprocess from 'svelte-preprocess';
 
 const mode = process.env.NODE_ENV;
-const dev = mode === "development";
+const dev = mode === 'development';
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
-const onwarn = (warning, onwarn) => (warning.code === "CIRCULAR_DEPENDENCY" && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
-const dedupe = importee => importee === "svelte" || importee.startsWith("svelte/");
+const onwarn = (warning, onwarn) =>
+  (warning.code === 'CIRCULAR_DEPENDENCY' &&
+    /[/\\]@sapper[/\\]/.test(warning.message)) ||
+  warning.message === 'Unused CSS selector' ||
+  onwarn(warning);
+const dedupe = (importee) =>
+  importee === 'svelte' || importee.startsWith('svelte/');
 
 const preprocess = sveltePreprocess({
   scss: {
-    includePaths: ["src"]
+    includePaths: ['src'],
   },
   postcss: {
-    plugins: [require("autoprefixer")]
-  }
+    plugins: [require('autoprefixer')],
+  },
 });
 
 export default {
@@ -34,51 +39,51 @@ export default {
       commonjs(),
       dotenvPlugin(),
       replace({
-        "process.browser": true,
-        "process.env.NODE_ENV": JSON.stringify(mode)
+        'process.browser': true,
+        'process.env.NODE_ENV': JSON.stringify(mode),
       }),
       resolve({
         browser: true,
-        dedupe
+        dedupe,
       }),
       svelte({
         dev,
         hydratable: true,
         preprocess,
-        emitCss: true
+        emitCss: true,
       }),
 
       legacy &&
         babel({
-          extensions: [".js", ".mjs", ".html", ".svelte"],
+          extensions: ['.js', '.mjs', '.html', '.svelte'],
           runtimeHelpers: true,
-          exclude: ["node_modules/@babel/**"],
+          exclude: ['node_modules/@babel/**'],
           presets: [
             [
-              "@babel/preset-env",
+              '@babel/preset-env',
               {
-                targets: "> 0.25%, not dead"
-              }
-            ]
+                targets: '> 0.25%, not dead',
+              },
+            ],
           ],
           plugins: [
-            "@babel/plugin-syntax-dynamic-import",
+            '@babel/plugin-syntax-dynamic-import',
             [
-              "@babel/plugin-transform-runtime",
+              '@babel/plugin-transform-runtime',
               {
-                useESModules: true
-              }
-            ]
-          ]
+                useESModules: true,
+              },
+            ],
+          ],
         }),
 
       !dev &&
         terser({
-          module: true
-        })
+          module: true,
+        }),
     ],
 
-    onwarn
+    onwarn,
   },
 
   server: {
@@ -88,21 +93,24 @@ export default {
       commonjs(),
       dotenvPlugin(),
       replace({
-        "process.browser": false,
-        "process.env.NODE_ENV": JSON.stringify(mode)
+        'process.browser': false,
+        'process.env.NODE_ENV': JSON.stringify(mode),
       }),
       resolve({
-        dedupe
+        dedupe,
       }),
       svelte({
-        generate: "ssr",
+        generate: 'ssr',
         preprocess,
-        dev
-      })
+        dev,
+      }),
     ],
-    external: Object.keys(pkg.dependencies).concat(require("module").builtinModules || Object.keys(process.binding("natives"))),
+    external: Object.keys(pkg.dependencies).concat(
+      require('module').builtinModules ||
+        Object.keys(process.binding('natives'))
+    ),
 
-    onwarn
+    onwarn,
   },
 
   serviceworker: {
@@ -113,12 +121,12 @@ export default {
       dotenvPlugin(),
       resolve(),
       replace({
-        "process.browser": true,
-        "process.env.NODE_ENV": JSON.stringify(mode)
+        'process.browser': true,
+        'process.env.NODE_ENV': JSON.stringify(mode),
       }),
-      !dev && terser()
+      !dev && terser(),
     ],
 
-    onwarn
-  }
+    onwarn,
+  },
 };
